985cffa763501a35e05df726f8410261c7c86ecd
Paweł Zawada <zawnet@pc253.home>
2021-12-11 11:21:54 +0100

    initial commit

---
+++ src/main/java/connector/OpenlibraryAPIConnector.java
@@ -0,0 +1,24 @@
+package connector;
+
+public class OpenlibraryAPIConnector {
+    private String baseUrl = "https://openlibrary.org";
+    private String endpoint;
+
+    public String getBaseUrl() {
+        return baseUrl;
+    }
+
+    public void setBaseUrl(String baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+
+    public String getEndpoint() {
+        return endpoint;
+    }
+
+    public void setEndpoint(String endpoint) {
+        this.endpoint = endpoint;
+    }
+
+
+}
---
+++ src/main/java/model/Doc.java
@@ -0,0 +1,33 @@
+package model;
+
+import java.util.List;
+
+public class Doc {
+
+    private String key;
+    private String type;
+    private List<String> seed;
+    private String title;
+    private String title_suggest;
+    private boolean has_fulltext;
+    private int edition_count;
+    private List<String>edition_key;
+    private List<String> publish_date;
+    private List<Integer> publish_year;
+    private int first_publish_year;
+    private int number_of_pages_median;
+    private List<String> isbn;
+    private int last_modified_i;
+    private int ebook_count_i;
+    private String cover_edition_key;
+    private int cover_i;
+    private List<String> publisher;
+    private List<String> author_key;
+    private List<String> author_name;
+    private List<String> author_alternative_name;
+    private List<String> id_amazon;
+    private List<String> publisher_facet;
+    private int _version_;
+    private List<String> author_facet;
+
+}
